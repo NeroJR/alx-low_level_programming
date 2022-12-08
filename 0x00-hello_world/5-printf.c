198be0cc44cb7e63463c6a7b9eb8dd3df815d76c
NeroJR <jrnetia@gmail.com>
2022-12-08 12:40:01 -0800

    Add comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/*
+ * main - prints to standard output using printf
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
