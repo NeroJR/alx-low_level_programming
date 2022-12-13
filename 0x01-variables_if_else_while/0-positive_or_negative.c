5921a2bd2df2318d4cc1e2f05508062c4ecdcbfd
NeroJR <jrnetia@gmail.com>
2022-12-13 07:25:20 -0800

    Loops

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,10 @@
 /**
  * more headers goes there
  * style doc for function main goes there
- * main - Assigns a random number to a variable 
- * States whether it is positive or negative
+ * main: Assigns a random number to a variable
+ * 	States whether it is positive or negative
  *
- * Returns 0
+ * Return: 0
  */
 int main(void)
 {
@@ -16,11 +16,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 
-	if (n>0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n<0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
