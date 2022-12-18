528ce0049a0d882f65b2f58920206e2c2e9a2d2e
NeroJR <jrnetia@gmail.com>
2022-12-18 06:42:48 -0800

    fibs

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - fibonacci upto 4m
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	unsigned long i = 0;
+	unsigned long j = 1;
+	unsigned long sum;
+	unsigned long rn_sum;
+
+	while (1)
+	{
+		rn_sum = i + j;
+
+		if (rn_sum > 4000000)
+			break;
+
+		if (rn_sum % 2 == 0)
+			sum = sum + rn_sum;
+
+		i = j;
+		j = rn_sum;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
