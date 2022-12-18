24b414b535c4da972801ea517e81dd67f164f3f6
NeroJR <jrnetia@gmail.com>
2022-12-18 04:34:30 -0800

    Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,15 +10,20 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar(i * j);
+			k = i * j;
+			_putchar(k);
 
 			for (j < 9)
-				_putchar(', ');
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
