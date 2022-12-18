2f1b67bc29154d53e0dc0d6af863be3e505474a5
NeroJR <jrnetia@gmail.com>
2022-12-18 06:26:16 -0800

    Fibs

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main - prints first 50 fibonacci
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	long int i = 0;
+	long int j = 1;
+	long int sum;
+	int count;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = i + j;
+		printf("%lu", sum);
+
+		i = j;
+		j = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+
+	}
+	return(0);
+}
