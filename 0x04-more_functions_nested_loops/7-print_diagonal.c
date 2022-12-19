8e0892b38f3725b5da2ced87dfe16eb9cbdb112b
NeroJR <jrnetia@gmail.com>
2022-12-19 11:32:39 -0800

    diagonala

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *print_diagonal - prints diagonals 
+ *@n: parameter passed to function
+ *Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int count = 0;
+
+	if (n > 0)
+	{
+		while (count < n)
+		{
+			count++;
+			i = count;
+
+			while (i > 1)
+			{
+				_putchar(' ');
+				i--;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
