2761d49c696cf02d3cb29cee4b7a9c8fac8a169c
NeroJR <jrnetia@gmail.com>
2022-12-19 13:44:04 -0800

    prints

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_rev - prints string in reverse
+ *@s: parameter
+ */
+
+void print_rev(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+		continue;
+
+	for (i--; i >= 0; i--)
+		_putchar(s[x]);
+
+	_putchar('\n');
+}
