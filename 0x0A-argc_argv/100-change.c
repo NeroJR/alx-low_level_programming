e82828c38e3b99feb863f6692f93558bf1fff8e0
NeroJR <jrnetia@gmail.com>
2023-01-22 00:00:27 -0800

    conets

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - change to give back
+ *@argc: size
+ *@argv: array
+ *Return: 0 if success, 1 if error
+ */
+
+int main(int argc, char **argv)
+{
+	int count = 0, total;
+	unsigned int i;
+	char *pt;
+	int coins[] = {25, 10, 5, 2};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	total = strtol(argv[1], &pt, 10);
+
+	if (!*pt)
+	{
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(coins[i]); i++)
+			{
+				if (total >= coins[i])
+				{
+					count += total / coins[i];
+					total = total % coins[i];
+				}
+			}
+		}
+
+		if (total == 1)
+			count++;
+	}
+
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	printf("%d\n", count);
+
+	return (0);
+}
