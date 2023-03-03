53c31c14c128cd8fb70d2df26002e91f24722e60
NeroJR <jrnetia@gmail.com>
2023-03-03 08:04:07 +0000

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ **_strcpy - final question
+ *@dest: parameter one
+ *@src: parameter two
+ *Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0'; x++)
+		dest[x] = src[x];
+
+	dest[x] = '\0';
+
+	return (dest);
+}
